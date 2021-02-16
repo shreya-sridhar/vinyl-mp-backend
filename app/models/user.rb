cfefd16d65dda145c354a7035214df4a06209ace
Alena Trushnikova <70681270+AlenaTrushnikova@users.noreply.github.com>
2021-02-15 17:15:08 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :sell_records
   accepts_nested_attributes_for :sell_records
   has_many :orders
-  # has_secure_password
   # validates :username, uniqueness: { case_sensitive: false }
 end
