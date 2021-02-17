2330f1b9d6f187686e9577f2183905e3f7466178
shreya-sridhar <shreyasridhar1109@gmail.com>
2021-02-16 16:29:48 -0800

    jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
-  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create]
 
-  # def profile
-  #   render json: { user: UserSerializer.new(current_user) }, status: :accepted
-  # end
-  #
-  # def create
-  #   @user = User.create(user_params)
-  #   if @user.valid?
-  #     @token = encode_token(user_id: @user.id)
-  #     render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-  #   else
-  #     render json: { error: 'failed to create user' }, status: :not_acceptable
-  #   end
-  # end
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+  
   def index
     render json: User.all
   end
